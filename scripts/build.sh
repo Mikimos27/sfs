0edc2fcb7945b5419733ff4af4e386cbd919644b
Mikołaj Modliński <mikimod27@gmail.com>
2025-04-30 16:14:50 +0200

    renames

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc src/sfs-server.c src/connection.c -o server.elf -Wall -Werror
-gcc src/sfs-client.c src/connection.c -o client.elf -Wall -Werror
+gcc src/server.c src/connection.c -o server.elf -Wall -Werror
+gcc src/client.c src/connection.c -o client.elf -Wall -Werror
 
