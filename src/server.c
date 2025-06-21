63c7f2cfa248f1c328c437d638d749f073b7daf7
Mikołaj Modliński <mikimod27@gmail.com>
2025-06-22 01:21:58 +0200

    proper resource release

--- src/server.c
+++ src/server.c
@@ -11,34 +11,40 @@
 #define BLOG 10
 #define TIMEOUT 1000
 
-char** filepath;
+
+void clean(Connection*);
+
 
 void net(Connection* server, const char* addr){
     
     if(server->socket < 0){
         perror("Failed to connect...\n");
+        clean(server);
         exit(1);
     }
     if(bind(server->socket, (struct sockaddr*)&server->address,
                 sizeof(server->address)) < 0){
         perror("Failed to bind...\n");
+        clean(server);
         exit(1);
     }
 
     if(listen(server->socket, server->backlog) < 0){
+        clean(server);
         perror("Failed to listen...\n");
         exit(1);
     }
 
 }
 
-void launch(Connection* server){
+void launch(Connection* server, const char* filepath){
     char buffer[B_MAX] = { 0 };
 
     int clientfd = accept(server->socket, 0, 0);
-    FILE* file = fopen(*filepath, "w");
+    FILE* file = fopen(filepath, "w");
     if(file == NULL){
         perror("CANT OPEN FILE\n");
+        close(clientfd);
         return;
     }
     while(recv(clientfd, buffer, B_MAX - 1, 0) != 0){
@@ -46,6 +52,7 @@ void launch(Connection* server){
         zero_arr(buffer, B_MAX);
     }
 
+    fclose(file);
     close(clientfd);
 
 }
@@ -63,9 +70,8 @@ int main(int argc, char** argv){
     Connection server = con_init(AF_INET, SOCK_STREAM, 0, INADDR_ANY, PORT, BLOG);
     
 
-    filepath = &argv[1];
     net(&server, 0);
-    launch(&server);
+    launch(&server, argv[1]);
     clean(&server);
     return 0;
 }
