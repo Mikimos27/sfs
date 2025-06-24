72109fb30f6cd987b8e11c8754cb22fa3512864b
Mikołaj Modliński <mikimod27@gmail.com>
2025-06-24 21:32:11 +0200

    implemented fstream instead of FILE*

--- src/client.cpp
+++ src/client.cpp
@@ -1,14 +1,15 @@
 #include "../hdr/connection.h"
 #include "../hdr/convinience.h"
-//#include "../hdr/getch.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+
+
 #include <arpa/inet.h>
 
 
 #include <string>
+#include <iostream>
+#include <fstream>
 
 
 #define B_MAX 3000
@@ -25,7 +26,7 @@ public:
 
    
     ~Client(){
-        printf("Client closed\n");
+        std::cout << "Client closed\n";
     }
     
 
@@ -38,7 +39,7 @@ public:
 private:
     int net(){
         if(inet_pton(con.domain, address.c_str(), &con.address.sin_addr) <= 0){
-            perror("Bad address given\n");
+            std::cerr << "Bad address given\n";
             return -1;
         }
         con.interface = inet_addr(address.c_str());
@@ -47,8 +48,8 @@ private:
 
 
     int launch(){
-        if(connect(con.sock, (struct sockaddr*)&con.address, sizeof(con.address)) < 0){
-            perror("Failed to connect to server\n");
+        if(connect(con.sock, (sockaddr*)&con.address, sizeof(con.address)) < 0){
+            std::cerr << "Failed to connect to server\n";
             return -1;
         } 
         return 1;
@@ -57,19 +58,18 @@ private:
 
     int manage(){
         char buffer[B_MAX] = { 0 };
-
-        FILE* file = fopen(filepath.c_str(), "r");
-        if(file == NULL){
-            perror("BAD FILE PATH\n");
+        
+        std::fstream file(filepath, std::ios::in);
+        if(!file){
+            std::cerr << "BAD FILE PATH\n";
             return -1;
         }
-
-        size_t chars_read = 0;
-        while((chars_read = fread(buffer, sizeof(char), B_MAX - 1, file)) > 0){
-            buffer[chars_read] = 0;
+        while(file){
+            file.read(buffer, B_MAX - 1);
             send(con.sock, buffer, B_MAX - 1, 0);
+            zero_arr(buffer, B_MAX);
         }
-        fclose(file);
+
         return 1;
     }
 
@@ -86,15 +86,15 @@ private:
 
 int main(int argc, char** argv){
     if(argc < 2) {
-        perror("No address given\n");
+        std::cerr << "No address given\n";
         return 1;
     }
     if(argc < 3){
-        perror("No filepath given\n");
+        std::cerr << "No filepath given\n";
         return 1;
     }
     if(argc < 4){
-        perror("No port given\n");
+        std::cerr << "No port given\n";
         return 1;
     }
 
@@ -103,7 +103,7 @@ int main(int argc, char** argv){
     int port = atoi(argv[3]);
 
     if(port < 0){
-        perror("Invalid port input\n");
+        std::cerr << "Invalid port input\n";
         return 1;
     }
 
