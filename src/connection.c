5d4a0eaae9180db48684faac1024b853b7ce26ad
Mikimos27 <mikimod27@gmail.com>
2025-09-01 13:36:13 +0200

    make more readable

---
+++ src/connection.c
@@ -0,0 +1,27 @@
+#include "../hdr/connection.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Connection con_init(int domain, int service, int protocol, unsigned long interface, int port, int backlog, void (*net)(Connection*, const char*), void (*launch)(Connection*)){
+    
+    Connection con;
+    con.domain = domain;
+    con.service = service;
+    con.protocol = protocol;
+    con.interface = interface;
+    con.port = port;
+    con.backlog = backlog;
+
+
+    con.address.sin_family = domain;
+    con.address.sin_port = htons(port);
+    con.address.sin_addr.s_addr = htonl(interface);
+
+    con.socket = socket(domain, service, protocol);
+
+    con.net = net;
+    con.launch = launch;
+
+    return con;
+}
+
