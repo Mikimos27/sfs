9a67cd13c874fa3f5feee3ee20dc1a3166239203
Mikimos27 <mikimod27@gmail.com>
2025-09-01 13:36:13 +0200

    Address validation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc src/sfs-server.c src/connection.c -o server.elf
-gcc src/sfs-client.c src/connection.c -o client.elf
+gcc src/sfs-server.c src/connection.c -o server.elf -Wall -Werror
+gcc src/sfs-client.c src/connection.c -o client.elf -Wall -Werror
 
--- src/sfs-client.c
+++ src/sfs-client.c
@@ -5,14 +5,16 @@
 #include <poll.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include "convinience.c"
 
 #define B_MAX 3000
 #define PORT 5050
 #define BLOG 10
 #define TIMEOUT 1000
+#define MAX_ADDRLEN 15
+
 
 void net(Connection* client, const char* addr){
-    printf("%s\n", addr);
     if(inet_pton(client->domain, addr, &client->address.sin_addr) <= 0){
         perror("Failed net connection...");
         exit(1);
@@ -44,13 +46,36 @@ void launch(Connection* client){
 }
 
 
+
+int addr_validation(const char* str){
+    int block_counts[4] = {0};
+    int dot_num = 0;
+    int addrlen = strlen(str);
+
+    for(int i = 0; i < addrlen; i++){
+        if(str[i] == '.') { 
+            dot_num++;
+            continue;
+        }
+        if(str[0] < '0' || str[i] > '9') return 1;
+        if(dot_num > 3) return 1;
+        block_counts[dot_num]++;
+    }
+
+    for(int i = 0; i < 4; i++){
+        if(block_counts[i] == 0 || block_counts[i] > 3) return 1;
+    }
+    if(dot_num > 3) return 1;
+    return 0;
+}
+
 int main(int argc, char** argv){
     if(argc < 2) {
-        printf("No address given");
+        printf("No address given\n");
         exit(1);
     }
-    if(strlen(argv[1]) > 15){
-        printf("Address too long");
+    if(addr_validation(argv[1])){
+        printf("Bad address\n");
         exit(1);
     }
 
