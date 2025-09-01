504ed29cfff5d50fde92ab4e6d925b7a6652fc0f
Mikimos27 <mikimod27@gmail.com>
2025-09-01 13:36:13 +0200

    set zero to char array

--- hdr/convinience.h
+++ hdr/convinience.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stddef.h>
 
 int64_t max(int64_t a, int64_t b){
     return a > b ? a : b;
@@ -6,4 +7,11 @@ int64_t max(int64_t a, int64_t b){
 
 int64_t min(int64_t a, int64_t b){
     return a < b ? a : b;
+}
+
+
+void zero_arr(char* c, size_t size){
+    for(size_t i = 0; i < size; i++){
+        c[0] = 0;
+    }
 }
