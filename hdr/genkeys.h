5cc4ccc3fa29b9fa9daa162b98e3a86ea685b5f3
Mikimos27 <mikimod27@gmail.com>
2025-09-01 13:36:13 +0200

    generate key pair tests

--- hdr/genkeys.h
+++ hdr/genkeys.h
@@ -1,8 +1,14 @@
 #ifndef KEYGEN_H
 #define KEYGEN_H
+#include <openssl/rsa.h>
+#include <unistd.h>
 
+void genkeypair(){
+    int kfd = open("../keys.txt", O_CREAT);
 
-void genkeypair(
+    EVP_PKEY* keys = EVP_RSA_gen(8);
+    free(keys);
+}
 
 
 #endif
